7f5eb26f9c54ef6bfb25782f595600e83be7cb7f
QuentinPrc <quentin.parc295@gmail.com>
2025-04-13 20:58:49 +0200

    Mise à jour des fichiers

--- Web/123.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for i in {1..10}; do
-  echo "Requête $i:"
-  curl -s http://figaro.localhost | grep Hostname
-  sleep 1
-done
--- Web/entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-echo "<h2>Hostname: $(hostname)</h2>" > /usr/share/nginx/html/hostname.html
-nginx -g 'daemon off;'
